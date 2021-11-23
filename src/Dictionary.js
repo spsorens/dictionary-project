a75d780486682bfb1b2635259e1180ac0b126cb6
spsorens <spsorens@gmail.com>
2021-11-22 20:58:39 -0800

    updated search bar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+  function handleKeywordChange(event) {
+    console.log(event);
+    setKeyword(event.target.value);
+  }
+
   return (
-    <div>
-      <h2>hello from dictionary</h2>
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
     </div>
   );
 }
