9b2c502af9df918be70b18ee53b6073178d7b062
spsorens <spsorens@gmail.com>
2021-11-27 12:51:58 -0800

    added Results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,12 +7,13 @@ export default function Dictionary() {
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
   }
   function search(event) {
     event.preventDefault();
 
     //documentation https://dictionaryapi.dev/
-    let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/`${keyword}`";
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
