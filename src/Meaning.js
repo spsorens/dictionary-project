c10cbcdc408aa30cbb1356662c2bcf417e605b5f
spsorens <spsorens@gmail.com>
2021-11-27 20:15:21 -0800

    updated meanings component

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,8 +5,17 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.parOfSpeech}</h3>
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
