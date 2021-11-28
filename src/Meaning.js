b14a56a6ce1966c0b35c702f79284e903afc42c4
spsorens <spsorens@gmail.com>
2021-11-27 20:06:58 -0800

    added Meanings component

---
+++ src/Meaning.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.parOfSpeech}</h3>
+      <p>{props.meaning.definitions[0].definition}</p>
+      <p>{props.meaning.definitions[0].example}</p>
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   if (props.result) {
-    return <div className="Results">Hello from Results</div>;
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        console.log(props.results.word);
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
