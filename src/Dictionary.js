4a854c5f267968e788560e2ea889fe6efd5f7029
spsorens <spsorens@gmail.com>
2021-12-14 20:28:55 -0800

    added Photos and Bootstrap

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     //documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000010c1d22721195412f818288a6bd572a3d";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+    let headers = { Authorization: `bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -42,6 +55,7 @@ export default function Dictionary(props) {
           <div className="hint">suggested words: dance, sing, soul...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,12 +3,12 @@ import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
-    <div className="Phonetic mt-1">
-      <a className="mb-3" href={props.phonetic.audio} target="_blank">
+    <div className="Phonetic">
+      <a className href={props.phonetic.audio} target="_blank">
         Listen
       </a>
       <br />
-      <span className="text mt-5">{props.phonetic.text}</span>
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
